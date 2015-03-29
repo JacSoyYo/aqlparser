5e40c7ed221219f2cc97bd49d6fbf9b44b2d6cb1
JacSoyYo <chamorro.j@gmail.com>
2015-03-29 13:08:04 +0200

    AqlListener, alreade deprecated

--- src/main/java/es/sacyl/jacsoyyo/aqlparser/AqlListener.java
+++ src/main/java/es/sacyl/jacsoyyo/aqlparser/AqlListener.java
@@ -4,8 +4,16 @@
 package es.sacyl.jacsoyyo.aqlparser;
 
 import es.jacsoyyo.aqlparser.AqlBaseListener;
+import es.jacsoyyo.aqlparser.AqlParser.ArchetypedClassExprContext;
+import es.jacsoyyo.aqlparser.AqlParser.ContainsContext;
+import es.jacsoyyo.aqlparser.AqlParser.EhrContainsContext;
+import es.jacsoyyo.aqlparser.AqlParser.FromContext;
+import es.jacsoyyo.aqlparser.AqlParser.FromEHRContext;
+import es.jacsoyyo.aqlparser.AqlParser.OrderByContext;
 import es.jacsoyyo.aqlparser.AqlParser.SelectExprContext;
 import es.jacsoyyo.aqlparser.AqlParser.SelectVarContext;
+import es.jacsoyyo.aqlparser.AqlParser.SimpleClassExprContext;
+import es.jacsoyyo.aqlparser.AqlParser.WhereContext;
 
 /**
  * @author jacobo
@@ -22,26 +30,62 @@ public class AqlListener extends AqlBaseListener {
 
 	@Override
 	public void enterSelectExpr(SelectExprContext ctx) {
-		System.out.println("Empieza la select");
+		System.out.println("SELECT");
 		super.enterSelectExpr(ctx);
 	}
 
 	@Override
-	public void exitSelectExpr(SelectExprContext ctx) {
-		System.out.println("Termina la select");
-		super.exitSelectExpr(ctx);
-	}
-
-	@Override
 	public void enterSelectVar(SelectVarContext ctx) {
-		System.out.println("Empieza una select var");
+		System.out.println("\tselectVar");
 		super.enterSelectVar(ctx);
 	}
 
 	@Override
-	public void exitSelectVar(SelectVarContext ctx) {
-		System.out.println("Termina una select var");
-		super.exitSelectVar(ctx);
+	public void enterFrom(FromContext ctx) {
+		System.out.println("FROM");
+		super.enterFrom(ctx);
+	}
+
+	@Override
+	public void enterEhrContains(EhrContainsContext ctx) {
+		System.out.println("\tEHR CONTAINS");
+		super.enterEhrContains(ctx);
+	}
+
+	@Override
+	public void enterFromEHR(FromEHRContext ctx) {
+		System.out.println("\t\tFROM EHR: " + ctx.IDENTIFIER().getText());
+		super.enterFromEHR(ctx);
+	}
+
+	@Override
+	public void enterContains(ContainsContext ctx) {
+		System.out.println("\t\tCONTAINS");
+		super.enterContains(ctx);
+	}
+
+	@Override
+	public void enterSimpleClassExpr(SimpleClassExprContext ctx) {
+		System.out.println("\t\t\tSIMPLE CLASS EXPRESSION");
+		super.enterSimpleClassExpr(ctx);
+	}
+
+	@Override
+	public void enterArchetypedClassExpr(ArchetypedClassExprContext ctx) {
+		System.out.println("\t\t\t\t" + ctx.IDENTIFIER(0));
+		super.enterArchetypedClassExpr(ctx);
+	}
+
+	@Override
+	public void enterWhere(WhereContext ctx) {
+		System.out.println("WHERE");
+		super.enterWhere(ctx);
+	}
+
+	@Override
+	public void enterOrderBy(OrderByContext ctx) {
+		System.out.println("ORDER BY");
+		super.enterOrderBy(ctx);
 	}
 
 }
