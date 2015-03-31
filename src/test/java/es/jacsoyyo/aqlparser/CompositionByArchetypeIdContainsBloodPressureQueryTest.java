5ec929a25d6cc1c8a0e59f1f67c21488acff8ce9
JacSoyYo <chamorro.j@gmail.com>
2015-03-31 14:28:28 +0200

    Few more TODO tests

---
+++ src/test/java/es/jacsoyyo/aqlparser/CompositionByArchetypeIdContainsBloodPressureQueryTest.java
@@ -0,0 +1,15 @@
+package es.jacsoyyo.aqlparser;
+
+public class CompositionByArchetypeIdContainsBloodPressureQueryTest extends
+		AbstractAqlQueryTest {
+
+	@Override
+	protected String aqlQueryString() {
+		return "SELECT "
+				+ "o/data[at0001]/events[at0006]/data[at0003]/items[at0004]/value/magnitude, "
+				+ "o/data[at0001]/events[at0006]/data[at0003]/items[at0005]/value/magnitude "
+				+ "FROM EHR [ehr_id/value='1234'] CONTAINS COMPOSITION c [openEHR-EHR-COMPOSITION.encounter.v1] CONTAINS "
+				+ "OBSERVATION o [openEHR-EHR-OBSERVATION.blood_pressure.v1]";
+	}
+
+}
