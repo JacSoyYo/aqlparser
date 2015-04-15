0e503b16217efcb906e2816a8438dba6e546c4e1
JacSoyYo <chamorro.j@gmail.com>
2015-04-15 20:23:38 +0200

    Change in first test query

--- src/test/java/es/jacsoyyo/aqlparser/CompositionByArchetypeIdContainsBloodPressureQueryTest.java
+++ src/test/java/es/jacsoyyo/aqlparser/CompositionByArchetypeIdContainsBloodPressureQueryTest.java
@@ -7,10 +7,10 @@ public class CompositionByArchetypeIdContainsBloodPressureQueryTest extends
 	protected String aqlQueryString() {
 		return "SELECT "
 				+ "c/uid/value, "
-				+ "o/data[at0001]/events[at0006]/data[at0003]/items[at0004]/value/magnitude, "
-				+ "o/data[at0001]/events[at0006]/data[at0003]/items[at0005]/value/magnitude "
+				+ "evn/data[at0003]/items[at0004]/value/magnitude, "
+				+ "evn/data[at0001]/events[at0006]/data[at0003]/items[at0005]/value/magnitude "
 				+ "FROM EHR [ehr_id/value='1234'] CONTAINS COMPOSITION c [openEHR-EHR-COMPOSITION.encounter.v1] CONTAINS "
-				+ "OBSERVATION o [openEHR-EHR-OBSERVATION.blood_pressure.v1]";
+				+ "OBSERVATION o [openEHR-EHR-OBSERVATION.blood_pressure.v1] CONTAINS EVENT evn";
 	}
 
 	// @formatter:off
